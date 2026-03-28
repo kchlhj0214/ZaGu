12c2da7509de1f8a99282f98d891f4a5d53a6c3e
kchlhj0214 <wlsdlf117@naver.com>
2026-03-28 19:18:25 +0900

    자구
    자구

--- Proj_05_kch/Proj_05_kch/Proj_05_kch.c
+++ Proj_05_kch/Proj_05_kch/Proj_05_kch.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define PROB 1 // 각각의 문제를 구현하고 해당 문제 번호를 변경하여 테스트
+#define PROB 2 // 각각의 문제를 구현하고 해당 문제 번호를 변경하여 테스트
 #if PROB == 1
 // 1번 문제를 해결하기 위한 프로그램 작성
 
@@ -209,6 +209,165 @@ int main()
 #elif PROB == 2
 // 2번 문제를 해결하기 위한 프로그램 작성
 
+typedef struct {
+	int x;
+	int y;
+}Pos;
 
+typedef Pos element;
+
+typedef struct {
+	element* data;
+	int capacity;
+	int top;
+}StackType;
+
+void init_stack(StackType* s)
+{
+	s->top = -1;
+	s->capacity = 1;
+	s->data = (element*)malloc(s->capacity * sizeof(element));
+}
+
+int is_empty(StackType* s) {
+	return (s->top == -1);
+}
+
+int is_full(StackType* s) {
+	return (s->top == (s->capacity - 1));
+}
+
+void push(StackType* s, element item)
+{
+	if (is_full(s)) {
+		s->capacity *= 2;
+		s->data = (element*)realloc(s->data, s->capacity * sizeof(element));
+	}
+	s->data[++(s->top)] = item;
+}
+
+element pop(StackType* s)
+{
+	if (is_empty(s)) {
+		exit(1);
+	}
+	else
+		return s->data[(s->top)--];
+}
+
+element peek(StackType* s)
+{
+	if (is_empty(s)) {
+		exit(1);
+	}
+	else
+		return s->data[s->top];
+}
+
+void reverse(StackType* s) {
+	StackType temp;
+	init_stack(&temp);
+	while (!is_empty(s)) {
+		push(&temp, pop(s));
+	}
+	free(s->data);
+	*s = temp;
+}
+
+#define MAZE_SIZE 6
+char maze[MAZE_SIZE][MAZE_SIZE] = {
+{'e', '0', '1', '1', '1', '1'},
+{'1', '0', '1', '0', '0', '1'},
+{'1', '0', '0', '0', '1', '1'},
+{'1', '0', '1', '0', '1', '1'},
+{'1', '0', '1', '0', '0', 'x'},
+{'1', '1', '1', '1', '1', '1'},
+};
+
+//char maze[MAZE_SIZE][MAZE_SIZE] = {
+//{'1', '1', '1', '1', '1', '1'},
+//{'0', '0', '1', '0', '0', '1'},
+//{'1', '0', '1', '0', '1', '1'},
+//{'1', '0', '1', '0', '1', '1'},
+//{'1', '0', '1', '0', '0', '0'},
+//{'e', '0', '0', '0', '1', 'x'},
+//};
+
+void Escape_Maze(StackType* s) {
+	Pos mouse;
+	Pos exit;
+
+	for (int i = 0; i < MAZE_SIZE; ++i) {
+		for (int j = 0; j < MAZE_SIZE; ++j) {
+			if (maze[i][j] == 'e') {
+				mouse.x = i;
+				mouse.y = j;
+			}
+			else if (maze[i][j] == 'x') {
+				exit.x = i;
+				exit.y = j;
+			}
+		}
+	}
+	Pos curr = mouse;
+	push(s, curr);
+
+	while (!is_empty(s)) {
+		curr = peek(s); // 현재 위치에서 사방을 확인
+		int x = curr.x;
+		int y = curr.y;
+
+
+		if (maze[x][y] == 'x') return;
+		maze[x][y] = '.';
+
+		// 1. 상우하좌 중 갈 수 있는 곳(0 또는 x)이 있는지 하나씩 확인
+		if (x > 0 && (maze[x - 1][y] == '0' || maze[x - 1][y] == 'x')) {
+			Pos next = { x - 1, y }; push(s, next);
+		}
+		else if (y < MAZE_SIZE - 1 && (maze[x][y + 1] == '0' || maze[x][y + 1] == 'x')) {
+			Pos next = { x, y + 1 }; push(s, next);
+		}
+		else if (x < MAZE_SIZE - 1 && (maze[x + 1][y] == '0' || maze[x + 1][y] == 'x')) {
+			Pos next = { x + 1, y }; push(s, next);
+		}
+		else if (y > 0 && (maze[x][y - 1] == '0' || maze[x][y - 1] == 'x')) {
+			Pos next = { x, y - 1 }; push(s, next);
+		}
+		else {
+			pop(s);
+		}
+	}
+}
+
+int main()
+{
+	StackType s;
+	init_stack(&s);
+
+	printf("탐색 전\n--------------------\n");
+	for (int i = 0; i < MAZE_SIZE; ++i) {
+		for (int j = 0; j < MAZE_SIZE; ++j) {
+			printf(" %c", maze[i][j]);
+		}
+		printf("\n");
+	}
+	printf("--------------------\n탐색 종료 후\n--------------------\n");
+	Escape_Maze(&s);
+
+	reverse(&s);
+
+	for (int i = 0; i < MAZE_SIZE; ++i) {
+		for (int j = 0; j < MAZE_SIZE; ++j) {
+			printf(" %c", maze[i][j]);
+		}
+		printf("\n");
+	}
+	printf("--------------------\n경로 :\n(row   col)\n");
+	while (!is_empty(&s)) {
+		Pos way = pop(&s);
+		printf("(%d   %d)\n", way.x, way.y);
+	}
+}
 
 #endif
